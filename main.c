039d2d3f55f18a05e259d308a92e96399bc4f264
schuppeste <philmajestic@hotmail.com>
2020-12-20 16:35:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     any later version.
-    Foobar is distributed in the hope that it will be useful,
+    S5Parser is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with S5Parser.  If not, see <http://www.gnu.org/licenses/>.
     Diese Datei ist Teil von S5Parser.
     S5Parser ist Freie Software: Sie können es unter den Bedingungen
     der GNU General Public License, wie von der Free Software Foundation,
